506488721935739128fe692ef69303aee42d11db
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-03 13:04:05 +0530

    clerk setup

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,24 @@
+import { SignIn, ClerkLoading, ClerkLoaded } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <div className='w-full min-h-screen flex flex-col items-center justify-between bg-black bg-[url("/image.png")] bg-cover bg-center gap-0'>
+        <span className='w-full '>
+          <Link href='/'>
+            <Image src='/logo.svg' width={100} height={100} alt='logo' className='hover:z-20'/>
+          </Link>  
+        </span>
+        <SignIn />
+        <span className='flex gap-2 text-slate-300 text-xs   mb-5'>
+          <Link href='/privacy' className='hover:underline hover:text-white'>
+            Privacy
+          </Link>
+          <Link href='/contact' className='hover:underline hover:text-white'>
+           Contact
+          </Link>
+        </span>
+    </div>
+  )
+}
---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+const isApiRoute = createRouteMatcher(['/api(.*)', '/trpc(.*)', '/dashboard(.*)'])
+
+export default clerkMiddleware((auth, request) => {
+  if (isApiRoute(request)) {
+    auth().protect()
+  }
+})
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
