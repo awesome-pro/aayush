8be4b20bc4f5bd8615c0f760f7f6056f70a9c261
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-04 18:12:05 +0530

    doctor update

--- app/api/doctor/create/route.ts
+++ app/api/doctor/create/route.ts
@@ -1,34 +1,40 @@
 import { NextResponse, NextRequest } from "next/server";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import DoctorModel from "@/backend/models/doctor";
+import dbConnect from "@/lib/db-connect";
 
 
-export default async function POST(req: NextRequest) {
-    const user = await currentUser();
+export async function POST(req: NextRequest) {
+    //const user = await currentUser();
 
+    const { name, email, phoneNumber, departments, hospital, roomNumber, qualifications, experience, consultationFee, availability, bio, rating } = await req.json();
+
+    try {
+        await dbConnect();
+        const newDoctor = new DoctorModel({
+            name: name,
+            email: email,
+            phoneNumber:  phoneNumber, 
+            departments,
+            hospital,
+            roomNumber,
+            qualifications,
+            experience,
+            consultationFee,
+            availability,
+            bio,
+            rating
+        }); 
     
-
-    const { departments, hospital, roomNumber, qualifications, experience, consultationFee, availability, bio, rating } = await req.json();
-
-    const newDoctor = new DoctorModel({
-        name: user?.fullName,
-        email: user?.emailAddresses[0].emailAddress,
-        phoneNumber: user?.phoneNumbers[0].phoneNumber, 
-        departments,
-        hospital,
-        roomNumber,
-        qualifications,
-        experience,
-        consultationFee,
-        availability,
-        bio,
-        rating
-    }); 
-
-    const response = await newDoctor.save();
-    if (response) {
-        return NextResponse.json({ message: "Doctor created successfully" }, {status: 200});
+        const response = await newDoctor.save();
+        console.log(response);
+        if (response) {
+            return NextResponse.json({ message: "Doctor created successfully" }, {status: 200});
+        }
+    
+        return NextResponse.json({ message: "Doctor creation failed" }, {status: 400});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Error creating doctor" }, {status: 400});
     }
-
-    return NextResponse.json({ message: "Doctor creation failed" }, {status: 400});
 }
