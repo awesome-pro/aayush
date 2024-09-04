ea88d5791fba6daa2fb7537b2319b410b976b923
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-04 14:32:03 +0530

    doctor backend complete

--- app/api/doctor/create/route.ts
+++ app/api/doctor/create/route.ts
@@ -1,3 +1,32 @@
 import { NextResponse, NextRequest } from "next/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import DoctorModel from "@/backend/models/doctor";
 
 
+export default async function POST(req: NextRequest) {
+    const user = await currentUser();
+
+    const { departments, hospital, roomNumber, qualifications, experience, consultationFee, availability, bio, rating } = await req.json();
+
+    const newDoctor = new DoctorModel({
+        name: user?.fullName,
+        email: user?.emailAddresses[0].emailAddress,
+        phoneNumber: user?.phoneNumbers[0].phoneNumber, 
+        departments,
+        hospital,
+        roomNumber,
+        qualifications,
+        experience,
+        consultationFee,
+        availability,
+        bio,
+        rating
+    }); 
+
+    const response = await newDoctor.save();
+    if (response) {
+        return NextResponse.json({ message: "Doctor created successfully" }, {status: 200});
+    }
+
+    return NextResponse.json({ message: "Doctor creation failed" }, {status: 400});
+}
