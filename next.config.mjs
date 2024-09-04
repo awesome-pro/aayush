a269bfa9c0a13c7a2668961ccdab7a9c466f7f08
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-04 12:20:08 +0530

    image update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = async () => {
+    const { default: withVideos } = await import('next-videos');
+    
+    return withVideos({
+        reactStrictMode: true,
+        images: {
+            remotePatterns: [
+                {
+                    hostname: 'res.cloudinary.com',
+                },
+                {
+                    hostname: 'images.unsplash.com',
+                },
+                {
+                    hostname: 'via.placeholder.com',
+                },
+                {
+                    hostname: 'www.google.com'
+                },
+                {
+                    hostname: 's3-alpha-sig.figma.com'
+                },
+                {
+                    hostname: 'www.pexels.com'
+                },
+                {
+                    hostname: 'img.pikbest.com'
+                },
+                {
+                    hostname: 'plus.unsplash.com'
+                }
+            ]
+        },
+        // your other Next.js config options here
+    });
+};
 export default nextConfig;
